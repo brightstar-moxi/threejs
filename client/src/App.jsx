a04aaff26cf79458d361c6a1417638ada6408235
brightstar-moxi <brightstarmoxiz2@gmail.com>
2023-07-30 03:05:40 +0100

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
+      <h1 className="head-text">
       Hello world!
     </h1>
     </>
