709e0efa30a757191991ed7c3c15d71e4d39c555
brightstar-moxi <brightstarmoxiz2@gmail.com>
2023-07-30 03:16:03 +0100

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,12 @@
+import Canvas from './canvas';
+import Customizer from './pages/Customizer';
+import Home from './pages/Home'
 
 function App() {
   return (
     <>
       <h1 className="head-text">
-      Hello world!
+    ThreeJs
     </h1>
     </>
   )
