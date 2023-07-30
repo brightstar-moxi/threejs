04c284e61f9863358247ee39ea9afad1647d7ccf
brightstar-moxi <brightstarmoxiz2@gmail.com>
2023-07-30 04:09:16 +0100

    update

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -24,6 +24,15 @@ const Home = () => {
           className='w-8 h-8 object-contain'
           />
       </motion.header>
+
+      <motion.div className= "home-content" {...headContainerAnimation}>
+        <motion.div {...headTextAnimation}>
+          <h1 className='head-text'>
+           LET'S <br className='xl:block hidden'/> DO IT.
+          </h1>
+        </motion.div>
+
+      </motion.div>
         </motion.section >
       )}
     </AnimatePresence>
